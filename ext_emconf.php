cc5e33a3b19e7b0874b8440267278cbe062822ff
Kay Strobach <info@kay-strobach.de>
2019-11-07 20:35:19 +0100

    [TASK] set all ext versions compatible to v9 for zips
    drk-intern/typo3-main-distribution#117

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '9.4.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.17-9.5.99',
+            'typo3' => '9.5.0-9.5.99',
             'news' => '7.3.0-7.3.99',
         ),
         'conflicts' => array(
