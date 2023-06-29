eb44e4b349d57e5eac36435342830621664700aa
Kay Strobach <info@kay-strobach.de>
2023-06-29 13:08:22 +0200

    Update ext_tables.php remove TYPO3_MODE

--- ext_tables.php
+++ ext_tables.php
@@ -3,27 +3,25 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
+/**
+ * Registers a Backend Module
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'Newssync',
+    'site',	 // Make module a submodule of 'tools'
+    'sync',	// Submodule key
+    '',						// Position
+    array(
+        \Fourviewture\Newssync\Controller\SyncConfigurationController::class => 'list, show, refreshData',
 
-    /**
-     * Registers a Backend Module
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Newssync',
-        'site',	 // Make module a submodule of 'tools'
-        'sync',	// Submodule key
-        '',						// Position
-        array(
-            \Fourviewture\Newssync\Controller\SyncConfigurationController::class => 'list, show, refreshData',
+    ),
+    array(
+        'access' => 'user,group',
+        'icon'   => 'EXT:newssync/Resources/Public/Icons/module_syncconfiguration.svg',
+        'labels' => 'LLL:EXT:newssync/Resources/Private/Language/locallang_sync.xlf',
+    )
+);
 
-        ),
-        array(
-            'access' => 'user,group',
-            'icon'   => 'EXT:newssync/Resources/Public/Icons/module_syncconfiguration.svg',
-            'labels' => 'LLL:EXT:newssync/Resources/Private/Language/locallang_sync.xlf',
-        )
-    );
-}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_newssync_domain_model_syncconfiguration', 'EXT:newssync/Resources/Private/Language/locallang_csh_tx_newssync_domain_model_syncconfiguration.xlf');
 
