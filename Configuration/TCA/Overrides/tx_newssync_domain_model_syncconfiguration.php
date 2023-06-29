65a0d18392240cdd341fdc660f57f07d53bf03eb
Kay Strobach <info@kay-strobach.de>
2023-06-29 16:59:25 +0200

    [TASK] v12 updates
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- Configuration/TCA/Overrides/tx_newssync_domain_model_syncconfiguration.php
+++ Configuration/TCA/Overrides/tx_newssync_domain_model_syncconfiguration.php
@@ -1,16 +1,3 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-    'newssync',
-    'tx_newssync_domain_model_syncconfiguration',
-    // optional: in case the field would need a different name as "categories".
-    // The field is mandatory for TCEmain to work internally.
-    'categories',
-    // optional: add TCA options which controls how the field is displayed. e.g position and name of the category tree.
-    [
-        'position' => 'after:news_is_top_news',
-        'fieldList' => 'categories'
-    ]
-);
-
 $GLOBALS['TCA']['tx_newssync_domain_model_syncconfiguration']['columns']['news_type'] = $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['type'];
--- ext_tables.php
+++ ext_tables.php
@@ -3,26 +3,6 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-/**
- * Registers a Backend Module
- */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'Newssync',
-    'site',	 // Make module a submodule of 'tools'
-    'sync',	// Submodule key
-    '',						// Position
-    array(
-        \Fourviewture\Newssync\Controller\SyncConfigurationController::class => 'list, show, refreshData',
-
-    ),
-    array(
-        'access' => 'user,group',
-        'icon'   => 'EXT:newssync/Resources/Public/Icons/module_syncconfiguration.svg',
-        'labels' => 'LLL:EXT:newssync/Resources/Private/Language/locallang_sync.xlf',
-    )
-);
-
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_newssync_domain_model_syncconfiguration', 'EXT:newssync/Resources/Private/Language/locallang_csh_tx_newssync_domain_model_syncconfiguration.xlf');
 
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
