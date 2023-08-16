0eae4188292fbcfc865592de8c55db29808cba4e
Kay Strobach <info@kay-strobach.de>
2023-08-16 09:56:39 +0200

    [BUGFIX] accept newstype update from settings

--- Classes/Services/Provider/AbstractImportService.php
+++ Classes/Services/Provider/AbstractImportService.php
@@ -281,7 +281,7 @@ class AbstractImportService
         $news->setImportSource(static::IMPORT_ID);
         $news->setImportId($syncKey);
         $news->setPid($syncConfiguration->getStoragePid());
-        $news->setType(0);
+        $news->setType($syncConfiguration->getNewsType() ?? 0);
         $news->setCrdate(new \DateTime());
         $news->setDatetime(new \DateTime());
         $news->setType($syncConfiguration->getNewsType());
