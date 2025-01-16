a6dabe06059530a195c38b2f9c1d4cb9e6a6f86a
Kay Strobach <info@kay-strobach.de>
2025-01-16 18:08:07 +0100

    [BUGFIX] localconf undefined constant
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,5 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['newssync']['importservices'][]
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['newssync']
     = \Fourviewture\Newssync\Hooks\T3libTcemainHook::class . '->clearCachePostProc';
 
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][]
-        = \Fourviewture\Newssync\Command\SyncAllCommandController::class;
-}
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][]
+    = \Fourviewture\Newssync\Command\SyncAllCommandController::class;
